257bc6d2a03a54204ed5c599488caf2a1804a1ee
Andrew Shabailov <andrewshabailov@gmail.com>
2026-08-11 23:24:15 +0300

    added negative tests to test_credits and test_login, added known_bug mark to pytest.ini

--- src/main/api/tests/test_credits.py
+++ src/main/api/tests/test_credits.py
@@ -1,3 +1,8 @@
+import pytest
+from src.main.api.config import CREDIT_REQUEST, CREDIT_REPAY, CREDIT_HISTORY
+from src.main.api.utils.request import post, get
+
+
 class TestCredit:
     def test_credit_request(self, credit_request, create_credit_user_account):
         assert credit_request.json()["id"] == create_credit_user_account.json()["id"]
@@ -8,5 +13,48 @@ class TestCredit:
 
     def test_credit_history(self, credit_request, credit_history):
         credit_id = credit_request.json()["creditId"]
-        print(credit_history.json())
         assert credit_id in [credit["creditId"] for credit in credit_history.json()["credits"]]
+
+    @pytest.mark.known_bug('This test should return 422 status code. 400 - temporary solution')
+    def test_credit_request_negative(self, create_credit_user_account, credit_token):
+        response = post(
+            CREDIT_REQUEST,
+            {
+                "accountId": create_credit_user_account.json()["id"],
+                "amount": 25000,
+                "termMonths": 12
+            },
+            token=credit_token,
+            expected_status=400
+        )
+        assert response.json()["error"] == "Amount must be between 5000 and 15000"
+
+    @pytest.mark.known_bug(
+        "This test returned wrong error."
+        "Expected message - 'Repayment amount exceeds remaining debt'"
+    )
+    def test_credit_repay_negative(self, credit_request, create_credit_user_account, credit_token):
+        response = post(
+            CREDIT_REPAY,
+            json={
+                "creditId": credit_request.json()["creditId"],
+                "accountId": create_credit_user_account.json()["id"],
+                "amount": 1
+            },
+            token=credit_token,
+            expected_status=422
+        )
+        assert response.json()["error"] == (f"The amount is not enough."
+                                            f" Credit balance: -{credit_request.json()['balance']}")
+
+    @pytest.mark.known_bug(
+        "This test returned wrong error."
+        "Expected message - 'Access denied'")
+    def test_credit_history_negative(self, admin_token):
+        response = get(
+            CREDIT_HISTORY,
+            token=admin_token,
+            expected_status=403
+        )
+        assert '403 Forbidden' in response.text
+
--- src/main/api/tests/test_login.py
+++ src/main/api/tests/test_login.py
@@ -1,6 +1,7 @@
+import pytest
 from src.main.api.utils.request import post
 from src.main.api.config import AUTH_LOGIN
-from src.main.api.data.data import ADMIN_USERNAME, ROLE_ADMIN, ADMIN_PASSWORD
+from src.main.api.data.data import ADMIN_USERNAME, ROLE_ADMIN, ADMIN_PASSWORD, USER_PASSWORD
 
 
 class TestLogin:
@@ -17,3 +18,37 @@ class TestLogin:
     def test_login_user(self, login_user, user_name):
         assert 'token' in login_user.json()
         assert login_user.json()["user"]["username"] == user_name, f"Username is NOT equal {user_name}"
+
+    @pytest.mark.parametrize("invalid_user_name, invalid_user_password",
+        [
+            ("абв", "Pas!sw0rd"),
+            ("ab", "Pas!sw0rd"),
+            ("abv!", "Pas!sw0rd"),
+            ("Max1", "Pas!sw0гд"),
+            ("Maxx2", "Pas!sw0"),
+            ("Maxx3", "pas!sw0rd"),
+            ("Maxx4", "PAS!SWORD"),
+            ("Maxx5", "PASSSWORD"),
+            ("Maxx6", "PAS!SWRRD")
+        ]
+                             )
+    def test_login_negative(
+            self,
+            invalid_user_name,
+            invalid_user_password
+    ):
+        response = post(
+            AUTH_LOGIN,
+            json={"username": invalid_user_name, "password": invalid_user_password},
+            expected_status=401
+        )
+        assert response.json()["error"] == "Invalid credentials"
+
+    def test_login_admin_negative(self):
+        response = post(
+            AUTH_LOGIN,
+            json={"username": ADMIN_USERNAME, "password": USER_PASSWORD},
+            expected_status=401
+        )
+        assert response.json()["error"] == "Invalid credentials"
+
