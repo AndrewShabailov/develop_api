b713fcc80fd87c0565728b516d6c85179da3dbf5
Andrew Shabailov <andrewshabailov@gmail.com>
2026-08-17 19:50:18 +0300

    added connection to db and test to check created user

--- src/main/api/tests/admin_users_test.py
+++ src/main/api/tests/admin_users_test.py
@@ -1,12 +1,15 @@
 import pytest
 
 from src.main.api.classes.api_manager import ApiManager
+from src.main.api.fixtures.db_fixture import db_session
 from src.main.api.foundation.endpoint import Endpoint
 from src.main.api.foundation.requesters.crud_requester import CrudRequester
 from src.main.api.generators.model_generator import RandomModelGenerator
 from src.main.api.models.create_user_request import CreateUserRequest
 from src.main.api.specs.request_specs import RequestSpecs
 from src.main.api.specs.response_specs import ResponseSpecs
+from src.main.api.db.crud.user_crud import UserCrudDb as User
+from sqlalchemy.orm.session import Session
 
 
 class TestAdmin:
@@ -14,11 +17,14 @@ class TestAdmin:
         "create_user_request",
         [RandomModelGenerator.generate(CreateUserRequest)]
     )
-    def test_create_user_valid(self, api_manager: ApiManager, create_user_request: CreateUserRequest):
+    def test_create_user_valid(self, api_manager: ApiManager, create_user_request: CreateUserRequest, db_session: Session):
         response = api_manager.admin_steps.create_user(create_user_request)
 
         assert create_user_request.username == response.username
         assert create_user_request.role == response.role
+
+        user_from_db = User.get_user_by_username(db_session, create_user_request.username)
+        assert user_from_db.username == create_user_request.username
 
     @pytest.mark.parametrize(
         "username, password",
