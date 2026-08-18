52b7409ef40c2c26c1000ab349d9c239e6852045
Andrew Shabailov <andrewshabailov@gmail.com>
2026-08-18 19:55:54 +0300

    covered all user tests with checking DB

--- src/main/api/tests/user_login_test.py
+++ src/main/api/tests/user_login_test.py
@@ -1,5 +1,5 @@
 import pytest
-
+from sqlalchemy.orm.session import Session
 from src.main.api.classes.api_manager import ApiManager
 from src.main.api.foundation.endpoint import Endpoint
 from src.main.api.foundation.requesters.crud_requester import CrudRequester
@@ -8,18 +8,32 @@ from src.main.api.models.create_user_request import CreateUserRequest
 from src.main.api.models.login_user_request import LoginUserRequest
 from src.main.api.specs.request_specs import RequestSpecs
 from src.main.api.specs.response_specs import ResponseSpecs
+from src.main.api.db.crud.user_crud import UserCrudDb as User
 
 
 @pytest.mark.api
 class TestUserLogin:
-    def test_login_admin(self, api_manager: ApiManager):
+    def test_login_admin(
+            self,
+            db_session: Session,
+            api_manager: ApiManager):
         login_user_request = LoginUserRequest(username="admin", password="123456")
         response = api_manager.admin_steps.login_user(login_user_request)
 
         assert login_user_request.username == response.user.username
         assert response.user.role == "ROLE_ADMIN"
 
-    def test_login_user(self, api_manager: ApiManager):
+        user_from_db = User.get_user_by_username(db_session, response.user.username)
+
+        assert user_from_db.username == response.user.username, f"Username is NOT equal {response.user.username}"
+        assert user_from_db.role == response.user.role, f"Role is NOT equal {response.role}"
+
+
+    def test_login_user(
+            self,
+            db_session: Session,
+            api_manager: ApiManager
+    ):
         user = RandomModelGenerator.generate(CreateUserRequest)
         api_manager.admin_steps.create_user(user)
         login_user_request = LoginUserRequest(username=user.username, password=user.password)
@@ -30,6 +44,11 @@ class TestUserLogin:
         assert 'token' in response.model_dump_json()
         assert user.username == response.user.username,\
             f"Username is NOT equal {user.username}"
+
+        user_from_db = User.get_user_by_username(db_session, response.user.username)
+
+        assert user_from_db.username == response.user.username, f"Username is NOT equal {response.user.username}"
+        assert user_from_db.role == response.user.role, f"Role is NOT equal {response.role}"
 
     @pytest.mark.known_bug("Response has wrong error message. Expected: 'Missing username or password'")
     @pytest.mark.parametrize(
