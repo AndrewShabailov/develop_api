7eff01cf8c54cd6ebdbbab03604c87eec943aa86
Andrew Shabailov <andrewshabailov@gmail.com>
2026-08-18 14:58:34 +0300

    covered all admin tests with checking DB

--- src/main/api/db/crud/user_crud.py
+++ src/main/api/db/crud/user_crud.py
@@ -8,6 +8,18 @@ class UserCrudDb:
         return db.query(User).filter_by(username=username).first()
 
     @staticmethod
+    def get_user_by_id(db: Session, id: int) -> User | None:
+        return db.query(User).filter_by(id=id).first()
+
+    @staticmethod
+    def get_user_by_role(db: Session, role: str) -> User | None:
+        return db.query(User).filter_by(role=role).first()
+
+    @staticmethod
+    def has_unique_name(db: Session, username: str) -> User | None:
+        return db.query(User).filter_by(username=username).all()
+
+    @staticmethod
     def create_user(db: Session, username: str, password: str, role: str) -> User:
         user = User(
             username=username,
--- src/main/api/tests/admin_users_test.py
+++ src/main/api/tests/admin_users_test.py
@@ -24,7 +24,10 @@ class TestAdmin:
         assert create_user_request.role == response.role
 
         user_from_db = User.get_user_by_username(db_session, create_user_request.username)
-        assert user_from_db.username == create_user_request.username, "Created user in not in DB"
+        role_from_db = User.get_user_by_role(db_session, create_user_request.role)
+
+        assert user_from_db.username == create_user_request.username, "Created user not in DB"
+        assert role_from_db.role == create_user_request.role, "Created user role not in DB"
 
     @pytest.mark.parametrize(
         "username, password",
@@ -48,6 +51,7 @@ class TestAdmin:
         )
         api_manager.admin_steps.create_invalid_user(create_user_request)
         user_from_db = User.get_user_by_username(db_session, create_user_request.username)
+
         assert user_from_db is None, "User is created"
 
     def test_all_users(
@@ -63,12 +67,23 @@ class TestAdmin:
         "create_user_request",
         [RandomModelGenerator.generate(CreateUserRequest)]
     )
-    def test_delete_user(self, api_manager: ApiManager, create_user_request: CreateUserRequest):
+    def test_delete_user(
+            self,
+            db_session: Session,
+            api_manager: ApiManager,
+            create_user_request: CreateUserRequest
+    ):
+
         created_user = api_manager.admin_steps.create_user(create_user_request)
         delete_response = api_manager.admin_steps.delete_user(created_user.id)
 
         assert delete_response.json()["message"] == "User deleted successfully", \
             f"Message: {delete_response.json()}"
+
+        user_from_db = User.get_user_by_id(db_session, created_user.id)
+
+        assert user_from_db is not None, f"User with ID {created_user.id} not found in database"
+        assert user_from_db.deleted_at is not None, f"User with ID {created_user.id} was not soft-deleted"
 
     @pytest.mark.parametrize(
         "create_user_request",
@@ -76,21 +91,36 @@ class TestAdmin:
     )
     def test_delete_all_users(
             self,
+            db_session: Session,
             api_manager: ApiManager,
             create_user_request: CreateUserRequest
     ):
 
-        api_manager.admin_steps.create_user(create_user_request)
+        created_user = api_manager.admin_steps.create_user(create_user_request)
         delete_result = api_manager.admin_steps.delete_all_users().json()
 
         assert delete_result["message"] == "All users except current admin deleted successfully"
+
+        user_from_db = User.get_user_by_id(db_session, created_user.id)
+        assert user_from_db is not None, f"User not found in DB"
+        assert user_from_db.deleted_at is not None, f"User with ID {created_user.id} was not soft-deleted!"
+
+        admin_from_db = User.get_user_by_username(db_session, api_manager.admin_steps.username)
+        assert admin_from_db is not None, "Current admin not found in database"
+        assert admin_from_db.deleted_at is None, "Current admin was incorrectly deleted!"
+
 
     @pytest.mark.known_bug('Response has wrong error message. Expected: "User already exists"')
     @pytest.mark.parametrize(
         "create_user_request",
         [RandomModelGenerator.generate(CreateUserRequest)]
     )
-    def test_create_user_with_the_same_name_negative(self, api_manager: ApiManager, create_user_request: CreateUserRequest):
+    def test_create_user_with_the_same_name_negative(
+            self,
+            db_session: Session,
+            api_manager: ApiManager,
+            create_user_request: CreateUserRequest
+    ):
         api_manager.admin_steps.create_user(create_user_request)
 
         response = CrudRequester(
@@ -105,9 +135,17 @@ class TestAdmin:
         assert response.json()["error"] == "User already has maximum number of accounts(2)",\
             f"Unexpected error message: {response.json()['error']}"
 
+        users_in_db = User.has_unique_name(db_session, api_manager.admin_steps.username)
+
+        assert len(users_in_db) == 1,\
+            f"Error: duplicate user was created in the database! Count: {len(users_in_db)}"
+
     @pytest.mark.known_bug('Response has wrong status code, error message.'
                            'Expected: status - 403, error - Admin access required')
-    def test_negative_user_gets_list_of_all_users(self, create_user_request: CreateUserRequest):
+    def test_negative_user_gets_list_of_all_users(
+            self,
+            create_user_request: CreateUserRequest
+    ):
 
         response = CrudRequester(
             RequestSpecs.auth_headers(
@@ -122,7 +160,11 @@ class TestAdmin:
             f"Unexpected error message: {response.json()['error']}"
 
 
-    def test_delete_non_existing_user_negative(self, api_manager: ApiManager):
+    def test_delete_non_existing_user_negative(
+            self,
+            db_session: Session,
+            api_manager: ApiManager
+    ):
 
         response = CrudRequester(
             RequestSpecs.auth_headers(
@@ -135,6 +177,9 @@ class TestAdmin:
 
         assert response.json()["error"] == "User not found",\
             f"Unexpected error: {response.json()['error']}"
+
+        nonexisting_user = User.get_user_by_id(db_session, 0)
+        assert nonexisting_user is None, f"Non-existing user {nonexisting_user} was found in database"
 
     @pytest.mark.known_bug('Response has wrong status code, error message.'
                            'Expected: status - 403, error - Admin access required')
